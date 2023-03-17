01778963f55b95f3f92bb6722b09433d7ae9dea3
IlyaAlpatov <alpatov30@gmail.com>
2023-03-17 13:04:24 +0300

    добавлены данные по-умолчанию

---
+++ src/store/defaultCalendarData.js
@@ -0,0 +1,81 @@
+export const daysOfWeek = [
+  {
+    shortcutName: "Вс",
+    name: "Воскресенье",
+  },
+  {
+    shortcutName: "Пн",
+    name: "Понедельник",
+  },
+  {
+    shortcutName: "Вт",
+    name: "Вторник",
+  },
+  {
+    shortcutName: "Ср",
+    name: "Среда",
+  },
+  {
+    shortcutName: "Чт",
+    name: "Четверг",
+  },
+  {
+    shortcutName: "Пт",
+    name: "Пятница",
+  },
+  {
+    shortcutName: "Сб",
+    name: "Суббота",
+  },
+];
+
+export const listOfMonths = [
+  {
+    nameInGenitive: "Января",
+    name: "Январь",
+  },
+  {
+    nameInGenitive: "Февраля",
+    name: "Февраль",
+  },
+  {
+    nameInGenitive: "Марта",
+    name: "Март",
+  },
+  {
+    nameInGenitive: "Апреля",
+    name: "Апрель",
+  },
+  {
+    nameInGenitive: "Мая",
+    name: "Май",
+  },
+  {
+    nameInGenitive: "Июня",
+    name: "Июнь",
+  },
+  {
+    nameInGenitive: "Июля",
+    name: "Июль",
+  },
+  {
+    nameInGenitive: "Августа",
+    name: "Август",
+  },
+  {
+    nameInGenitive: "Сентября",
+    name: "Сентябрь",
+  },
+  {
+    nameInGenitive: "Октября",
+    name: "Октябрь",
+  },
+  {
+    nameInGenitive: "Ноября",
+    name: "Ноябрь",
+  },
+  {
+    nameInGenitive: "Декабря",
+    name: "Декабрь",
+  },
+];
