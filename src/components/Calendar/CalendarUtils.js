439d77caad179cf7fcc88aaadc1e666715f692ee
IlyaAlpatov <alpatov30@gmail.com>
2023-03-18 13:16:05 +0300

    добавлены методы для получения списка недель и дней

--- src/components/Calendar/CalendarUtils.js
+++ src/components/Calendar/CalendarUtils.js
@@ -1,22 +1,65 @@
 export const getWeeksWithDays = (date) => {
+  let weeksList = [];
+  let calendarDays = getCalendarDays(date);
+  const numberOfWeeks = Math.ceil(calendarDays.length / 7);
+
+  for (let i = 1; i <= numberOfWeeks; i++) {
+    let start = (i - 1) * 7;
+    let end = i * 7;
+    weeksList.push(calendarDays.slice(start, end));
+  }
+
+  return weeksList;
+};
+
+const getCalendarDays = (date) => {
   const listOfDays = [];
   const currentMonth = date.getMonth();
+  const currentDayOfMonth = date.getDate();
   let dateToCount = new Date(date);
   dateToCount.setMonth(currentMonth + 1, 1);
   dateToCount.setDate(dateToCount.getDate() - 1);
-  // const amountOfDays = dateToCount.getDate();
 
-  while (currentMonth === dateToCount.getMonth()) {
+  let isCycle = true;
+
+  while (isCycle) {
     let dayOfMonth = dateToCount.getDate();
     let dayOfWeek = dateToCount.getDay();
+    let isCurrentMonth = currentMonth === dateToCount.getMonth() ? true : false;
+    let isCurrentDay =
+      currentMonth === dateToCount.getMonth() &&
+      currentDayOfMonth === dayOfMonth
+        ? true
+        : false;
 
     listOfDays.push({
       dayOfWeek,
       dayOfMonth,
+      isCurrentMonth,
+      isCurrentDay,
     });
+
+    if (dayOfWeek === 1 && currentMonth !== dateToCount.getMonth()) {
+      isCycle = false;
+    }
 
     dateToCount.setDate(dateToCount.getDate() - 1);
   }
 
-  console.log(listOfDays);
+  let lastDayOfWeek = listOfDays[0].dayOfWeek;
+  let newDayOfMonth = 1;
+
+  for (let i = lastDayOfWeek + 1; i <= 7; i++) {
+    let dayOfWeek = i === 7 ? 0 : i;
+    listOfDays.unshift({
+      dayOfWeek,
+      dayOfMonth: newDayOfMonth,
+      isCurrentMonth: false,
+      isCurrentDay: false,
+    });
+
+    newDayOfMonth++;
+  }
+
+  return listOfDays.reverse();
 };
